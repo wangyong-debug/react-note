5ead069aba40675ecb253b52ba8abcbd79dd8f20
wangyong <869270569@qq.com.com>
2022-08-09 18:45:35 +0800

    事件绑定的初步学习

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'
-import App from'./01-base/04-组件的样式'
+import App from'./01-base/05-事件绑定-1'
 import { createRoot } from 'react-dom/client'
 
 const container =document.getElementById('root')
