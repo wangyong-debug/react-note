c06e79a42e686f361762dcba2fd2d3568be8a8c1
wangyong <869270569@qq.com.com>
2022-08-10 10:21:02 +0800

    react和原生js绑定事件的区别

--- myapp/src/01-base/06-事件绑定-2.js
+++ myapp/src/01-base/06-事件绑定-2.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-// this.handleClick后面加括号的话他会在页面加载的时候自动执行一次
-// 1标签内部写点击事件（适合简单事件） 2事件调用。普通函数和箭头函数（推荐箭头函数）
-// 不过箭头函数调用函数的时候内部被调用的函数要写（）
+//  this.handleClick后面加括号的话他会在页面加载的时候自动执行一次
+//  1标签内部写点击事件（适合简单事件） 2事件调用。普通函数和箭头函数（推荐箭头函数）
+//  不过箭头函数调用函数的时候内部被调用的函数要写（）
 
 //  call 改变this指向，自动执行函数
 //  apply 改变this指向，自动执行函数
 //  bind 改变this指向，自动执行函数，手动加()执行函数
 //  不能自动执行，所以在这里一般使用bind
+//  事件并不会将点击事件绑定到div的真实dom上，而是在document处监听所有支持的事件，当事件发生并且冒泡到document处，react将事件内容封装并且交给真正的处理函数运行
 export default class App extends Component {
     a=100
     render() {
