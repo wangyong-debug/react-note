677e007b17baa452afec51cef4c66b0c5976c9c5
wangyong-debug <869270569@qq.ocm>
2022-08-11 19:13:45 +0800

    解决提交失效的问题

--- myapp/src/01-base/maizuocomponent/Cinema.js
+++ myapp/src/01-base/maizuocomponent/Cinema.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Cinema extends Component {
-    // constructor(){
-    //     super()
-    //     axios.get("https://m.maizuo.com/gateway?cityId=440300&pageNum=1&pageSize=10&type=1&k=4012299").then(res=>{
-    //         console.log(res);
-    //     }).catch(err=>{
-    //         console.log(err);
-    //     })
-    // }
-
+export default class Cinema extends Component{
     render() {
         return (
             <div>
